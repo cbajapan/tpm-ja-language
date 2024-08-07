b3f379125256b7b3d33f7b338ae2cd77b1eec72f
yutaka-cba <yutaka.kihara@cba-japan.com>
2024-08-07 15:52:10 +0900

    Update nojs_lang.php

--- ja/nojs_lang.php
+++ ja/nojs_lang.php
@@ -2,9 +2,9 @@
 $lang = array (
 	// Language to use in headers of html pages (<html lang="XX">)
 	// See: https://www.w3.org/International/questions/qa-html-language-declarations.en
-	'html_lang'					=> 'エン', // = general_lang.php
+	'html_lang'					=> 'JA', // = general_lang.php
 
-	'nojs_activate_js'		=> 'ジャバスクリプトを有効にする',
+	'nojs_activate_js'		=> 'Javascriptを有効にする',
 	'nojs_explanation'		=> '<strong>チームパスワード管理者</strong>はJavascriptが有効なブラウザでのみ動作します。ブラウザのJavascriptを有効にしてください。',
-	'nojs_goto_tpm'			=> 'チームパスワード管理へ'
+	'nojs_goto_tpm'			=> 'Team Password Managerへ'
 );
